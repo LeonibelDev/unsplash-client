3cc3f541c53ae864fdb5f8140a6c5bc1f7f51b12
leonibel <leonibel.ramirez@gmail.com>
2023-05-29 21:37:42 -0400

    Button for different image qualities

--- app.py
+++ app.py
@@ -4,14 +4,19 @@ import os
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/', methods = ['GET', 'POST'])
+download_quality = {
+        "thumb": 200,
+        "small": 640,
+        "regular": 1920,
+        "original": "" 
+    }
+
+@app.route('/', methods = ['GET'])
 def home():
-    #get method here
-    if request.method == 'GET':
-        query = request.args.get('query', 'random')
-        page = int(request.args.get('page', 1))
-        result = search(query, page)
-        return render_template('index.html', result=result, query=query, page=page)
+    query = request.args.get('query', 'random')
+    page = int(request.args.get('page', 1))
+    result = search(query, page)
+    return render_template('index.html', result=result, query=query, page=page, quality=download_quality)
        
 
 @app.route('/search', methods = ['GET'])
@@ -19,13 +24,6 @@ def getImages():
     query = request.args.get('query', 'random')
     page = int(request.args.get('page', 1))
     result = search(query, page)
-    
-    download_quality = {
-        "thumb": 200,
-        "small": 640,
-        "regular": 1920,
-        "original": "" 
-    }
     return render_template('index.html', result=result, query=query, page=page, quality=download_quality)
   
 
